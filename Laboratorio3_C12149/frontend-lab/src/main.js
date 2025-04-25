aeb0aee965e936a3407f5b5d86a7e7e7bbeafdff
esteban <esteban.alp10@icloud.com>
2025-04-24 21:37:21 -0600

    Laboratorio #5 Se crea el la pagina de pais

--- Laboratorio3_C12149/frontend-lab/src/main.js
+++ Laboratorio3_C12149/frontend-lab/src/main.js
@@ -2,10 +2,14 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import { createRouter, createWebHistory } from 'vue-router';
 import ListaPaises from './components/ListaPaises.vue';
+import PaisForm from './components/PaisForm.vue';
 
 const router = createRouter({
     history: createWebHistory(),
-    routes: [{ path: '/', name: 'home', component: ListaPaises }],
+    routes: [
+        { path: '/', name: 'home', component: ListaPaises }, 
+        { path: '/pais', name: 'pais', component: PaisForm }, 
+    ],
 });
 
 createApp(App).use(router).mount("#app");
